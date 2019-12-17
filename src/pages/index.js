c7134021408afa53500f686e9a0ccc640a352ed6
ayrbox <srdx7@hotmail.com>
2019-12-17 14:11:51 +0000

    Wrap content inside container

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container } from 'reactstrap';
 
 import Layout from '../components/layout';
 import Carousel from '../components/carousel';
@@ -6,8 +7,10 @@ import Carousel from '../components/carousel';
 const IndexPage = () => {
     return (
         <Layout>
-            <Carousel />
-            <h1>Hello Gatsby</h1>
+            <Container>
+                <Carousel />
+                <h1>Heen's Restaurant Chinese Oriental Cuisine</h1>
+            </Container>
         </Layout>
     );
 }
