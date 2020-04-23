574709f28520c14c17e9d8e575faffc354daa968
ayrbox <srdx7@hotmail.com>
2020-04-23 14:45:02 +0100

    Add home page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 
 import Layout from "../components/layout";
 import Carousel from "../components/carousel";
@@ -8,8 +8,33 @@ const IndexPage = () => {
   return (
     <Layout>
       <Carousel />
-      <Container>
-        <h1>Heen's Restaurant Chinese Oriental Cuisine</h1>
+      <Container className="content-wrapper">
+        <Row>
+          <Col>
+            <h1>Welcome to Heens</h1>
+            <p className="lead">
+              We specialise in Peking and Szechuan cuisine. Our head chef of
+              over 30 years experience is proud to create our most popular
+              authentic dishes for you in traditional way. We are confident that
+              you will these tasteful dishes impossible to resist.
+            </p>
+
+            <p className="lead">
+              Our chef are very happy to cater for any of your individual tastes
+              which are not on the menu. We arrange special party menu for all
+              occasions
+            </p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <h1>Speical Events</h1>
+            <p className="lead">
+              With sufficient space to host an event of up to 70 people
+            </p>
+          </Col>
+        </Row>
       </Container>
     </Layout>
   );
